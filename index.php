ceefa3f1563fde9358ed403f641665a39020137d
Jon Sheafe <msckj@yahoo.com>
2016-10-11 21:50:51 -0600

    initial creation of aboutme page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>About Me</title>
+	</head>
+	<body>
+		<header>Juin "Jon" Sheafe</header>
+		<h1>Hello, World! :D</h1>
+	</body>
+</html>
