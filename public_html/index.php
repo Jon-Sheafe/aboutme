75db85f380056453e2835984601550d85097ea2c
Jon Sheafe <msckj@yahoo.com>
2016-10-17 09:28:28 -0600

    initial creation of aboutme page

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en">rhewis37
 	<head>
 		<meta charset="utf-8"/>
 		<title>About Me</title>
 	</head>
 	<body>
 		<header>Juin "Jon" Sheafe</header>
-		<h1>Father</h1>
-
+<p>I am enjoyed programing in Access. I used VBA to add additional functionallity to the database. I hope to get a job working as a coder or database administrator.</p>
 
 	</body>
 </html>
