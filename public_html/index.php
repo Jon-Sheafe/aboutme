a4c7ddd352e5da995d537a8f72775bfba2db5409
Jon Sheafe <msckj@yahoo.com>
2016-10-17 09:40:02 -0600

    initial creation of aboutme page

--- public_html/index.php
+++ public_html/index.php
@@ -7,8 +7,8 @@
 	<body>
 		<header>Juin "Jon" Sheafe</header>
 <p>I am enjoyed programing in Access. I used VBA to add additional functionallity to the database. I hope to get a job working as a coder or database administrator.</p>
-		<h2>Family</h2>
-		<p>I have been married for 9 years and I have a 20 month old. I love being a father. <img src="C:\Users\finef\Desktop\bootcamp\git\aboutme\img\V__0BFB.jpg" alt="family picture"></p>
+		<h2>Family</h2><br>
+		<p>I have been married for 9 years and I have a 20 month old. I love being a father. <img src="../img/V__0BFB.jpg" alt="family picture" sizes="100px"></p>
 
 	</body>
 </html>
